3e42ce357b6e5b274fbdb0254bdaa7fa348d9eaa
Alph4 <alph4@gmx.at>
2025-08-21 18:00:14 +0200

    BingoSync API update

--- BingoGoalPack2.cs
+++ BingoGoalPack2.cs
@@ -1,19 +1,25 @@
-﻿using Modding;
+﻿using BingoSync.CustomGoals;
+using BingoSync.Interfaces;
+using Modding;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
-using BingoSync.CustomGoals;
 
 
 namespace BingoGoalPack2 {
     public class BingoGoalPack2: Mod {
         new public string GetName() => "BingoGoalPack2";
-        public override string GetVersion() => "1.0.1.0";
-        public override int LoadPriority() => 8;
+        public override string GetVersion() => "1.1.0.0";
 
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) {
+            OrderedLoader.OnReadyForGoalsGameModes += SetupGoalsGameModes;
+        }
+
+        private void SetupGoalsGameModes(object _, EventArgs __)
+        {
             Assembly assembly = Assembly.GetExecutingAssembly();
 
             Dictionary<string, BingoGoal> myGoals = processEmbeddedJson(assembly, "Goals");
